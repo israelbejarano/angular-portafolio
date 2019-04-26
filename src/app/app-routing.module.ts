605b7bf6938a68634ad2c37cdf3e1877ddcde8b2
israel <israel.bejarano.a@gmail.com>
2019-04-26 23:40:06 +0200

    add home ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const app_routes: Routes = [
-    {path: '', component: PortafolioComponent},
+    {path: 'home', component: PortafolioComponent},
     {path: 'about', component: AboutComponent},
     {path: 'item', component: ItemComponent},
     {path: '**', pathMatch: 'full', redirectTo: ''}
