4f06925d41db648e892d31db8bcc7b1f44f161bd
israel <israel.bejarano.a@gmail.com>
2019-04-27 12:39:31 +0200

    uso de firebase para mostrar el equipo

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,27 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any[] = [];
   cargada = false;
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+   cargarInfo() {
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
       console.log(resp);
       this.cargada = true;
       this.info = resp;
     });
    }
+
+  cargarEquipo() {
+    this.http.get('https://angular-html-58698.firebaseio.com/equipo.json').subscribe((resp: any[]) => {
+      console.log(resp);
+      this.cargada = true;
+      this.equipo = resp;
+    });
+  }
 }
